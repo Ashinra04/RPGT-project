422b84487c4ddf0ae98603c60d88888d71e22603
Ashinra04 <revoemag0408@gmail.com>
2026-03-29 14:07:19 -0300

    Market terminado

--- src/TELAS/Shop/Shop.js
+++ src/TELAS/Shop/Shop.js
@@ -17,24 +17,24 @@ let InventarioJogador = {
     "DeathP": 0
 };
 
-let goldPlayer = 150;
-let ItemSelecinado = null;
+let goldPlayer = 120;
+let ItemSelecionado = null;
 
 const ItemsLoja = {
-    "Potion": { nome: "Potion", hp: 30, icon: '/imagens/Items/Potion.png', Preco: 10, descricao: "Uma poçao usada para regenerar o hp, cura 30 de HP" },
-    "Potionx2": { nome: "Potionx2", hp: 50, icon: '/imagens/Items/Potionx2.png', Preco: 40, descricao: "Uma poçao evoluida da poção base, usada para regenerar o hp, cura 50 de HP" },
-    "Energy": { nome: "Energy", mp: 30, icon: '/imagens/Items/Energy.png', Preco: 10, descricao: "Uma bebida usada para regenerar a mana, regenera 30 de mana" },
-    "Energyx2": { nome: "Energyx2", mp: 50, icon: '/imagens/Items/Energyx2.png', Preco: 10, descricao: "Uma poçao evoluida da energy base, usada para regenerar a mana, regenera 50 de mana" },
-    "Elixir": { nome: "Elixir", hp: "max", mp: "max", icon: '/imagens/Items/Elixir.png', Preco: 10, descricao: "Um Elixir conhecida regera ate membros decepados, cura toda a vida e mana" },
-    "Full_heal": { nome: "Full heal", hp: "max", icon: '/imagens/Items/Full heal.png', Preco: 10, descricao: "O conceito de perfeição, uma poção usada para regenerar todo o hp, cura todo o HP" },
-    "PoisonP": { nome: "Poisonp", poison: 30, icon: '/imagens/Items/PoisonP.png', Preco: 10, descricao: "Uma poção que contem essencia de veneno, usar esse item adiciona dano elemental de veneno no turno" },
-    "FreezeP": { nome: "FreezeP", freeze: 50, icon: '/imagens/Items/FreezeP.png', Preco: 10, descricao: "Uma poção que contem essencia de gelo, usar esse item adiciona dano elemental de gelo no turno" },
-    "FireP": { nome: "FireP", fire: 30, icon: '/imagens/Items/FireP.png', Preco: 10, descricao: "Uma poção que contem essencia de fogo, usar esse item adiciona dano elemental de fogo no turno" },
-    "ElectricP": { nome: "ElectricP", electric: 50, icon: '/imagens/Items/ElectricP.png', Preco: 10, descricao: "Uma poção que contem essencia de trovão, usar esse item adiciona dano elemental eletrico no turno" },
-    "Antidote": { nome: "Antidote", icon: '/imagens/Items/Antidote.png', Preco: 10, descricao: "Uma poção usada para limpar o veneno do corpo, usar esse item tira o status de envenenado" },
-    "Revive": { nome: "Revive", icon: '/imagens/Items/Revive.png', Preco: 10, descricao: "Use esse item para reviver um aliado em batalha, pode ser usada fora de combate tambem" },
-    "Torch": { nome: "Torch", icon: '/imagens/Items/Torch.png', Preco: 5, descricao: "Para vencer os espiritos da escuridao, usa-se fogo, uma ferramenta usada para afastar a escuridão" },
-    "DeathP": { nome:"Death potion", icon: '/imagens/Items/Death potion.png', Preco: 0, descricao: "Uma poçáo que contem a essencia da morte, usar esse item te mata, use quando a situaçao for dificil" }
+    "Potion": { nome: "Potion", hp: 30, icon: '/imagens/Items/Potion.png', Preco: 10, descricao: "Uma poçao usada para regenerar o hp, cura 30 de HP." },
+    "Potionx2": { nome: "Potionx2", hp: 50, icon: '/imagens/Items/Potionx2.png', Preco: 40, descricao: "Uma poçao melhorada, usada para regenerar o hp, cura 50 de HP." },
+    "Energy": { nome: "Energy", mp: 30, icon: '/imagens/Items/Energy.png', Preco: 10, descricao: "Uma bebida usada para regenerar a mana, regenera 30 de mana." },
+    "Energyx2": { nome: "Energyx2", mp: 50, icon: '/imagens/Items/Energyx2.png', Preco: 10, descricao: "Energy melhorada, usada para regenerar a mana, regenera 50 de mana." },
+    "Elixir": { nome: "Elixir", hp: "max", mp: "max", icon: '/imagens/Items/Elixir.png', Preco: 10, descricao: "Um Elixir que cura toda a vida e mana." },
+    "Full_heal": { nome: "Full heal", hp: "max", icon: '/imagens/Items/Full heal.png', Preco: 10, descricao: "Uma poção usada para regenerar todo o hp, cura todo o HP." },
+    "PoisonP": { nome: "Poisonp", poison: 30, icon: '/imagens/Items/PoisonP.png', Preco: 10, descricao: "Usar esse item adiciona dano elemental de veneno no turno." },
+    "FreezeP": { nome: "FreezeP", freeze: 50, icon: '/imagens/Items/FreezeP.png', Preco: 10, descricao: "Usar esse item adiciona dano elemental de gelo no turno." },
+    "FireP": { nome: "FireP", fire: 30, icon: '/imagens/Items/FireP.png', Preco: 10, descricao: "Usar esse item adiciona dano elemental de fogo no turno." },
+    "ElectricP": { nome: "ElectricP", electric: 50, icon: '/imagens/Items/ElectricP.png', Preco: 10, descricao: "Usar esse item adiciona dano elemental eletrico no turno." },
+    "Antidote": { nome: "Antidote", icon: '/imagens/Items/Antidote.png', Preco: 10, descricao: "Uma poção usada para limpar o status de envenenado." },
+    "Revive": { nome: "Revive", icon: '/imagens/Items/Revive.png', Preco: 10, descricao: "Um item capaz de reviver aliados mortos fora e dentro de combate." },
+    "Torch": { nome: "Torch", icon: '/imagens/Items/Torch.png', Preco: 5, descricao: "Uma ferramenta capaz de afastar a escuridão." },
+    "DeathP": { nome:"Death potion", icon: '/imagens/Items/Death potion.png', Preco: 0, descricao: "Uma poçáo que contem a essencia da morte, usar esse item te mata." }
 };
 
 window.mudarItem = function (idItem) {
@@ -48,34 +48,31 @@ window.mudarItem = function (idItem) {
     document.getElementById('NOMEdoITEM').innerText = itemEscolhido.nome;
     document.getElementById('ITEMDescricao').innerText = itemEscolhido.descricao;
     document.getElementById('QUANTIDADEPlayer').innerText = "Você tem: " + InventarioJogador[idItem];
+    document.getElementById('coinPlayer').innerText = "🪙 " + goldPlayer;
+    document.getElementById('PrecoItem').innerText = "Preço:  " + ItemsLoja[idItem].Preco;
   } else {
     console.error("Erro: O item " + idItem + " não existe no objeto ItemsLoja.");
   }
   
-  itemSelecionado = idItem;
+  ItemSelecionado = idItem;
 };
 
 window.comprarItem = function () {
-  if (itemSelecionado === null) {
+  
+  const ItemComprar = ItemSelecionado;
+  const ItemPreco = ItemsLoja[ItemComprar].Preco;
+  
+  if (ItemSelecionado === null) {
     alert("Selecione um item primeiro!");
     return;
   }
-
-  const item = ItemsLoja[itemSelecionado];
-
-
-  if (goldPlayer >= item.preco) {
-    
-    goldPlayer -= item.preco;
-    InventarioJogador[itemSelecionado] += 1;
-    
-    document.getElementById('quantidadePlayer').innerText = "Você tem: " + InventarioJogador[itemSelecionado];
-    
-    console.log(`Você comprou ${item.nome}. Ouro restante: ${ouroJogador}`);
-    
+  
+  if(goldPlayer >= ItemPreco) {
+    goldPlayer -= ItemPreco;
+    InventarioJogador[ItemComprar] += 1;
+    document.getElementById('coinPlayer').innerText = "🪙 " + goldPlayer;
+    document.getElementById('QUANTIDADEPlayer').innerText = "Você tem: " + InventarioJogador[ItemComprar];
   } else {
-    
-    alert("Ouro insuficiente para comprar " + item.nome + "!");
+    alert("Ouro insuficiente para comprar " + ItemComprar + "!");
   }
-  alert('ta fufando')
 };
