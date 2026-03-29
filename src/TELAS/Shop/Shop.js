cd5e99baa828d9afb151956ba005cd2a015e5483
Ashinra04 <revoemag0408@gmail.com>
2026-03-29 10:39:32 -0300

    terminando a tela market, faltando somente onsistwma de compra para estae 100% pronta

--- src/TELAS/Shop/Shop.js
+++ src/TELAS/Shop/Shop.js
@@ -1,30 +1,13 @@
 // A lista de itens com os atributos deles
-const ItemsLoja = {
-    "Potion": { nome: "Potion", hp: 30 },
-    "Potionx2": { nome: "Potionx2", hp: 50 },
-    "Energy": { nome: "Energy", mp: 30 },
-    "Energyx2": { nome: "Energyx2", mp: 50 },
-    "Elixir": { nome: "Elixir", hp: "max", mp: "max" },
-    "Full heal": { nome: "Full heal", hp: "max" },
-    "Poisonp": { nome: "Poisonp", poison: 30 },
-    "FreezeP": { nome: "FreezeP", freeze: 50 },
-    "FireP": { nome: "FireP", fire: 30 },
-    "ElectricP": { nome: "ElectricP", electric: 50 },
-    "Antidote": { nome: "Antidote" },
-    "Revive": { nome: "Revive" },
-    "Torch": { nome: "Torch" },
-    "DeathP": { nome:"Death potion" }
-};
 
-// Se isso era o inventário do jogador, mude o nome da variável para não repetir
 let InventarioJogador = {
-    "Potion": 0,
+    "Potion": 3,
     "Potionx2": 0,
-    "Energy": 0,
+    "Energy": 2,
     "Energyx2": 0,
     "Elixir": 0,
-    "Full heal": 0, // Aspas adicionadas aqui
-    "Poisonp": 0,
+    "Full_heal": 0,
+    "PoisonP": 0,
     "FreezeP": 0,
     "FireP": 0,
     "ElectricP": 0,
@@ -34,11 +17,65 @@ let InventarioJogador = {
     "DeathP": 0
 };
 
-// Correção dos parênteses na função
-window.comprarItem = function () {
-    console.log("Comprando item...");
+let goldPlayer = 150;
+let ItemSelecinado = null;
+
+const ItemsLoja = {
+    "Potion": { nome: "Potion", hp: 30, icon: '/imagens/Items/Potion.png', Preco: 10, descricao: "Uma poçao usada para regenerar o hp, cura 30 de HP" },
+    "Potionx2": { nome: "Potionx2", hp: 50, icon: '/imagens/Items/Potionx2.png', Preco: 40, descricao: "Uma poçao evoluida da poção base, usada para regenerar o hp, cura 50 de HP" },
+    "Energy": { nome: "Energy", mp: 30, icon: '/imagens/Items/Energy.png', Preco: 10, descricao: "Uma bebida usada para regenerar a mana, regenera 30 de mana" },
+    "Energyx2": { nome: "Energyx2", mp: 50, icon: '/imagens/Items/Energyx2.png', Preco: 10, descricao: "Uma poçao evoluida da energy base, usada para regenerar a mana, regenera 50 de mana" },
+    "Elixir": { nome: "Elixir", hp: "max", mp: "max", icon: '/imagens/Items/Elixir.png', Preco: 10, descricao: "Um Elixir conhecida regera ate membros decepados, cura toda a vida e mana" },
+    "Full_heal": { nome: "Full heal", hp: "max", icon: '/imagens/Items/Full heal.png', Preco: 10, descricao: "O conceito de perfeição, uma poção usada para regenerar todo o hp, cura todo o HP" },
+    "PoisonP": { nome: "Poisonp", poison: 30, icon: '/imagens/Items/PoisonP.png', Preco: 10, descricao: "Uma poção que contem essencia de veneno, usar esse item adiciona dano elemental de veneno no turno" },
+    "FreezeP": { nome: "FreezeP", freeze: 50, icon: '/imagens/Items/FreezeP.png', Preco: 10, descricao: "Uma poção que contem essencia de gelo, usar esse item adiciona dano elemental de gelo no turno" },
+    "FireP": { nome: "FireP", fire: 30, icon: '/imagens/Items/FireP.png', Preco: 10, descricao: "Uma poção que contem essencia de fogo, usar esse item adiciona dano elemental de fogo no turno" },
+    "ElectricP": { nome: "ElectricP", electric: 50, icon: '/imagens/Items/ElectricP.png', Preco: 10, descricao: "Uma poção que contem essencia de trovão, usar esse item adiciona dano elemental eletrico no turno" },
+    "Antidote": { nome: "Antidote", icon: '/imagens/Items/Antidote.png', Preco: 10, descricao: "Uma poção usada para limpar o veneno do corpo, usar esse item tira o status de envenenado" },
+    "Revive": { nome: "Revive", icon: '/imagens/Items/Revive.png', Preco: 10, descricao: "Use esse item para reviver um aliado em batalha, pode ser usada fora de combate tambem" },
+    "Torch": { nome: "Torch", icon: '/imagens/Items/Torch.png', Preco: 5, descricao: "Para vencer os espiritos da escuridao, usa-se fogo, uma ferramenta usada para afastar a escuridão" },
+    "DeathP": { nome:"Death potion", icon: '/imagens/Items/Death potion.png', Preco: 0, descricao: "Uma poçáo que contem a essencia da morte, usar esse item te mata, use quando a situaçao for dificil" }
 };
 
-window.mudarItem = function (Item) {
-    console.log('Item mudado:', Item);
+window.mudarItem = function (idItem) {
+
+  const imagemDoItem = document.querySelector('#ITEM-Icon img');
+  const itemEscolhido = ItemsLoja[idItem];
+  const quantidadeAtual = InventarioJogador[idItem];
+  
+  if (itemEscolhido) {
+    imagemDoItem.src = itemEscolhido.icon;
+    document.getElementById('NOMEdoITEM').innerText = itemEscolhido.nome;
+    document.getElementById('ITEMDescricao').innerText = itemEscolhido.descricao;
+    document.getElementById('QUANTIDADEPlayer').innerText = "Você tem: " + InventarioJogador[idItem];
+  } else {
+    console.error("Erro: O item " + idItem + " não existe no objeto ItemsLoja.");
+  }
+  
+  itemSelecionado = idItem;
+};
+
+window.comprarItem = function () {
+  if (itemSelecionado === null) {
+    alert("Selecione um item primeiro!");
+    return;
+  }
+
+  const item = ItemsLoja[itemSelecionado];
+
+
+  if (goldPlayer >= item.preco) {
+    
+    goldPlayer -= item.preco;
+    InventarioJogador[itemSelecionado] += 1;
+    
+    document.getElementById('quantidadePlayer').innerText = "Você tem: " + InventarioJogador[itemSelecionado];
+    
+    console.log(`Você comprou ${item.nome}. Ouro restante: ${ouroJogador}`);
+    
+  } else {
+    
+    alert("Ouro insuficiente para comprar " + item.nome + "!");
+  }
+  alert('ta fufando')
 };
--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
-import './style.css'
+// CSS DAS TELAS
 
+import './style.css'
 import './TELAS/Shop/Shop.css';
-import './TELAS/Shop/Shop.js';
 import './TELAS/Forja/Forja.css';
-import './TELAS/Forja/Forja.js';
 import './TELAS/Taverna/Taverna.css';
-import './TELAS/Taverna/Taverna.js';
 import './TELAS/Guilda/Guilda.css';
-import './TELAS/Guilda/Guilda.js';
 import './TELAS/Batalha/Batalha.css';
+
+// JS DAS TELAS
+import './TELAS/Shop/Shop.js';
+import './TELAS/Forja/Forja.js';
+import './TELAS/Taverna/Taverna.js';
+import './TELAS/Guilda/Guilda.js';
 import './TELAS/Batalha/Batalha.js';
+
+// HTML DAS TELAS
+import shopHtml from './TELAS/Shop/Shop.html?raw';
+import guildHtml from './TELAS/Guilda/Guilda.html?raw';
+import tavernHtml from './TELAS/Taverna/Taverna.html?raw';
+import forjaHtml from './TELAS/Forja/Forja.html?raw';
+const corpoDoSite = document.body;
+corpoDoSite.insertAdjacentHTML('beforeend', shopHtml);
+corpoDoSite.insertAdjacentHTML('beforeend', guildHtml);
+corpoDoSite.insertAdjacentHTML('beforeend', tavernHtml);
+corpoDoSite.insertAdjacentHTML('beforeend', forjaHtml);
 
 window.mudarTela = function (idParaMostrar) {
     const telas = document.querySelectorAll('.tela');
@@ -21,8 +35,4 @@ window.mudarTela = function (idParaMostrar) {
     } else {
         console.error("A tela " + idParaMostrar + " não foi encontrada!");
     }
-}
-
-let Inventario = {
-  
 }
