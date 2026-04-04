fa3a8cb9065a038a4175480b5e3a76c8a9aa85de
Ashinra04 <revoemag0408@gmail.com>
2026-04-04 12:53:33 -0300

    desenvolvendo a loja da forja

--- src/TELAS/Forja/Forja.js
+++ src/TELAS/Forja/Forja.js
@@ -1,3 +1,5 @@
+
+
 window.mudarTelaForja = function (idDaTelaAlvo) {
     const Forjas = document.querySelectorAll('.Forja');
     
@@ -10,4 +12,59 @@ window.mudarTelaForja = function (idDaTelaAlvo) {
     } else {
         console.error("A tela " + idDaTelaAlvo + " não foi encontrada!");
     }
+}
+
+let inventarioForja = {
+  Armas: [
+    "Iron_sword",
+    "Iron_lance",
+    "Iron_Shield",
+    "Iron_dagger",
+    "bow",
+    "pistol",
+    "rifle",
+    "axe",
+    "mace",
+    "mangual"
+  ],
+  Armors: {
+    "Leather_helmet": "/imagens/Forja/Leather_helmet.png",
+    "Leather_armor": "/imagens/Forja/Leather_armor.png",
+    "Leather_pants": "/imagens/Forja/Leather_pants.png",
+    "Leather_gloves": "/imagens/Forja/Leather_gloves.png",
+    "Leather_boots": "/imagens/Forja/Leather_boots.png"
+  },
+  acessorio: [
+    "ring",
+    "amulet",
+    "cinto"
+    ]
+};
+
+/* "Iron_armor",
+    "Iron_helmet",
+    "Iron_pants",
+    "Iron_gloves",
+    "Iron_boots", */
+
+window.categoriaId = function(idCategoria) {
+
+  if(idCategoria === weapon) {
+    forEach() {
+      document.createElement('div');
+      divItem.id = nome;
+    }
+  }
+  
+  else if(idCategoria === armors) {
+    
+  }
+  else if(idCategoria === accessories) {
+    
+  }
+  
+}
+
+window.DesenharForjaBuy = function() {
+  
 }
