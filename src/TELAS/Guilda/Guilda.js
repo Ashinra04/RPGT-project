555ae55ab894dc021ee8af2cbbb9e9e1be9e4c46
Ashinra04 <revoemag0408@gmail.com>
2026-04-16 22:45:04 -0300

    Desenvolvido a esteuturaçao do bestiario, e fazendo uma funcao que modela todoa o design, alem de faser um pop up parw moatrar as informacoes doa moantros

--- src/TELAS/Guilda/Guilda.js
+++ src/TELAS/Guilda/Guilda.js
@@ -11,34 +11,103 @@ window.mudarConteudo = function (idParaIr) {
 }
 
 window.MonstrosBestiario = {
-  Goblin: { habitat: '1 a 9 andar', },
-  Goblin_Mago: { habitat: '1 a 9 andar', },
-  Goblin_Rastreador : { habitat: '1 a 9 andar', },
-  Slime: { habitat: '1 a 9 andar', },
-  Slime_Flamejante: { habitat: '1 a 9 andar', },
-  Spider: { habitat: '1 a 9 andar', },
-  Kobold: { habitat: '11 a 19 andar', },
-  Kobold_Lanceiro: { habitat: '11 a 19 andar', },
-  Rat: { habitat: '11 a 19 andar', },
-  Rat_Toxico: { habitat: '11 a 19 andar', },
-  Bat: { habitat: '11 a 19 andar', },
-  Orc: { habitat: '11 a 19 andar', },
-  Orc_Berseker: { habitat: '11 a 19 andar', }
+  "Andar 1/9": { 
+    Goblin: { habitat: '1 a 9 andar', sprite: '/imagens/monstros/goblin.png' },
+    Goblin_Mago: { habitat: '1 a 9 andar', sprite: '/imagens/monstros/goblin.png' },
+    Goblin_Rastreador : { habitat: '1 a 9 andar', sprite: '/imagens/monstros/goblin.png' },
+    Slime: { habitat: '1 a 9 andar', sprite: '/imagens/monstros/slime.png' },
+    Slime_Flamejante: { habitat: '1 a 9 andar', sprite: '/imagens/monstros/slime.png' },
+    Spider: { habitat: '1 a 9 andar', sprite: '/imagens/monstros/spider.png' },
+  },
+  
+  "Andar 11/19": { 
+    Kobold: { habitat: '11 a 19 andar', sprite: '/imagens/monstros/spider.png' },
+    Kobold_Lanceiro: { habitat: '11 a 19 andar', sprite: '/imagens/monstros/spider.png' },
+    Rat: { habitat: '11 a 19 andar', sprite: '/imagens/monstros/spider.png' },
+    Rat_Toxico: { habitat: '11 a 19 andar', sprite: '/imagens/monstros/spider.png' },
+  },
+  
+  "Andar 21/29": { 
+    Bat: { habitat: '21 a 29 andar', sprite: '/imagens/monstros/spider.png' },
+    Orc: { habitat: '21 a 29 andar', sprite: '/imagens/monstros/spider.png' },
+    Orc_Berseker: { habitat: '21 a 29 andar', sprite: '/imagens/monstros/spider.png' }
+  },
+  
+  "Andar 31/39": { 
+    Bat1: { habitat: '31 a 39 andar', sprite: '/imagens/monstros/spider.png' },
+  },
+  
+  "Andar 41/49": { 
+    Bat2: { habitat: '31 a 39 andar', sprite: '/imagens/monstros/spider.png' },
+  },
+  
+  "Andar 51/59": { 
+    Bat3: { habitat: '31 a 39 andar', sprite: '/imagens/monstros/spider.png' },
+  },
+  
+  "Andar 61/69": { 
+    Bat4: { habitat: '31 a 39 andar', sprite: '/imagens/monstros/spider.png' },
+  },
+  
+  "Andar 71/79": { 
+    Bat5: { habitat: '31 a 39 andar', sprite: '/imagens/monstros/spider.png' },
+  },
+  
+  "Andar 81/89": { 
+    Bat6: { habitat: '31 a 39 andar', sprite: '/imagens/monstros/spider.png' },
+  },
+  
+  "Andar 91/99": { 
+    Bat6: { habitat: '31 a 39 andar', sprite: '/imagens/monstros/spider.png' },
+  }
+}
+
+window.PopBestiario = function(OcultarAparecer) {
+  if (OcultarAparecer === 'ocultar') {
+    document.getElementById('BestiarioPopUp').classList.add('oculto');
+  }
+  else if (OcultarAparecer === 'aparecer') {
+    document.getElementById('BestiarioPopUp').classList.remove('oculto');
+  }
 }
 
 window.criarBestiario = function() {
   let containerBestiario = document.getElementById('BestiarioBody');
   containerBestiario.innerHTML = '';
   
-  Object.keys(window.MonstrosBestiario).forEach(function(nomeDoMonstro) {
-    let card = document.createElement('div');
-    card.className = 'Cards';
-    card.id = nomeDoMonstro;
-    // card.style.background = `url('${window.SpritesMonstros[nomeDoMonstro].sprite}')`;
-    card.onclick = function() {
-      alert(nomeDoMonstro);
-    }
+  Object.keys(window.MonstrosBestiario).forEach(function(nomeDoAndar) {
     
-    containerBestiario.appendChild(card);
+    // Cria a div da linha (Andar)
+    let BestAndar = document.createElement('div');
+    BestAndar.className = 'BestiarioRows';
+    containerBestiario.appendChild(BestAndar);
+    
+    // Cria o título
+    let AndarTitle = document.createElement('div');
+    AndarTitle.className = 'BesTitles';
+    AndarTitle.innerText = nomeDoAndar;
+    BestAndar.appendChild(AndarTitle);
+    
+    // Cria o corpo
+    let AndarBody = document.createElement('div');
+    AndarBody.className = 'BestBody';
+    BestAndar.appendChild(AndarBody);
+    
+    // Cria os cards dos monstros
+    let monstrosDesteAndar = window.MonstrosBestiario[nomeDoAndar];
+    
+    Object.keys(monstrosDesteAndar).forEach(function(nomeDoMonstro) {
+      let cardMonster = document.createElement('div');
+      cardMonster.id = nomeDoMonstro;
+      cardMonster.classList.add('MonstroCard', 'imgContain');
+      
+      cardMonster.style.backgroundImage = `url('${monstrosDesteAndar[nomeDoMonstro].sprite}')`
+      
+      AndarBody.appendChild(cardMonster);
+      
+      cardMonster.onclick = function() {
+        window.PopBestiario('aparecer');
+      }
+    });
   });
 }
