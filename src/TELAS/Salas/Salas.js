4d61b5b62b1c7c7fe919f2620b6c4daff98f3286
Ashinra04 <revoemag0408@gmail.com>
2026-05-05 10:49:47 -0300

    diario

--- src/TELAS/Salas/Salas.js
+++ src/TELAS/Salas/Salas.js
@@ -182,6 +182,15 @@ window.GerarMapaCompleto = function() {
   console.log("Mapa Gerado com Sucesso!", window.SalasDoAndar);
 };
 
+window.GerarPerfilE = function() {
+  const PlayerPerfilIcon = document.getElementById('perfilPlayer');
+  const PlayerAliado1Icon = document.getElementById('perfilAliado1');
+  const PlayerAliado2Icon = document.getElementById('perfilAliado2');
+  PlayerPerfilIcon.style.backgroundImage = "url('/imagens/Sprites/wizard_retrato.png')";
+  PlayerAliado1Icon.style.backgroundImage = "url('/imagens/Sprites/wizard_retrato.png')";
+  PlayerAliado2Icon.style.backgroundImage = "url('/imagens/Sprites/wizard_retrato.png')";
+}
+
 // ========================================================
 // 2. FUNÇÃO DE GERAR AS PORTAS E COLOCAR OS EMOJIS
 // ========================================================
